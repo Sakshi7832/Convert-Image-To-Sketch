2adc38cbce61cc85979821ca2f939f9d87cf2fa9
Sakshi <84897436+Sakshi7832@users.noreply.github.com>
2022-09-19 19:01:11 +0530

    Add files via upload

---
+++ sketch.py
@@ -0,0 +1,5 @@
+#convert image to cartoon sketch 
+#you need to install :- pip install sketchify
+
+from sketchify import sketch
+sketch.normalsketch('C:/Users/dell/Desktop/girl.jpg','C:/Users/dell/Desktop','sketchgirl')
